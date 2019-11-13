58c46f8de3dee283629ecec39c05f1231dc52d5b
Cláudio Gil <claudio.f.gil@gmail.com>
2019-11-13 18:55:15 +0000

    Reviewed style of options page

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="options">
-    <h1>Brave Heart Extension Options</h1>
-    <div class="exclusions">
-      <p>Exclusions:</p>
+    <h1 class="header">Brave Heart Extension Options</h1>
+    <div class="group exclusions">
+      <h2>Exclusions</h2>
+      <p class="description">
+        List of URLs that where excluded and are no longer shown as part of the visited
+        web sites during the period. Restoring the URL makes that site reappear in lists
+        together with its current usage.
+      </p>
       <ul>
         <li v-for="url in sortedExcludedUrls" v-bind:key="url">
-          <span>{{ url }}</span>
-          <a v-on:click="removeFromExcludedUrls(url)">x</a>
+          <img :src="'chrome://favicon/size/12/' + url" />
+          <span class="text">{{ url }}</span>
+          <span class="actions">
+            <a v-on:click="removeFromExcludedUrls(url)">
+              <fa-icon icon="trash-restore"/>
+              Restore
+            </a>
+          </span>
         </li>
       </ul>
     </div>
