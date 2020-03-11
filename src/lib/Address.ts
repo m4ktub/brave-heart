6170c4cf2a4eac166c0b6b92b8935ea4dd1d8119
Cláudio Gil <claudio.f.gil@gmail.com>
2020-03-11 15:06:58 +0000

    Replace cashaddrjs dependency with bitbox

--- src/lib/Address.ts
+++ src/lib/Address.ts
@@ -1,6 +1,6 @@
 import { CashAccount } from './CashAccount';
 import { asyncMapFind } from "./Functions";
-import * as bchaddr from "bchaddrjs";
+import { Address as BitboxAddress } from 'bitbox-sdk';
 
 export type Address = string;
 
@@ -9,10 +9,11 @@ export class AddressExtractor {
   private static AddrRegExp = new RegExp("(bitcoincash:)?[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{42,}|(BITCOINCASH:)?[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{42,}", "g");
   private static AccountRegExp = new RegExp(CashAccount.RegExp, "g");
   private static CashAccountResolver = new CashAccount();
+  private static AddressUtil = new BitboxAddress();
 
  async extract(text: string): Promise<Address> {
     const addressMatches = text.match(AddressExtractor.AddrRegExp) || [];
-    const address = addressMatches.find(bchaddr.isCashAddress) || null;
+    const address = addressMatches.find(AddressExtractor.AddressUtil.isCashAddress) || null;
     if (address) {
       return address;
     }
